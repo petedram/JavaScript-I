d93ea1a1c831269c4378a27711c3c63132cac5b6
Pete James <petejames@petes-air.lan>
2019-10-02 20:09:30 -0700

    improved function stretch

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -48,7 +48,13 @@ console.log(subtract(1,2));
 // const triple = exampleArray.map(function (num) { return num * 3;});
 // console.log(triple);
 
+
+//working version function outside
+// exampleArray = [1,2,3,4];
+// let tripfunc = (num) => num * 3;
+// const triple = exampleArray.map(tripfunc);
+// console.log(triple);
+
 exampleArray = [1,2,3,4];
-let tripfunc = (num) => num * 3;
-const triple = exampleArray.map(tripfunc);
+const triple = exampleArray.map((num) => num * 3);
 console.log(triple);
